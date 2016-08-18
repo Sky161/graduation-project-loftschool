0a058d3f2ac61a31620317dc64b4a58b32129437
Andrey Chechkin <andrey.chechkin@me.com>
2016-08-18 17:07:50 +0300

    Start Project

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+'use strict';
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var watch = require('gulp-watch');
+var jade = require('gulp-jade');
+
+var tplPath = './';
+
+gulp.task('scripts', function() {
+
+	gulp.src(tplPath + 'scripts/main.js')
+		.pipe(browserify({
+			shim: require("./browserify-shim.json")
+		}))
+		.pipe(plumber())
+		.pipe(rename("build.js"))
+		.pipe(gulp.dest(tplPath +'/app/'))
+});
+
+gulp.task('jade', function () {
+	gulp.src(tplPath + '/jade/*.jade')
+		.pipe(plumber())
+		.pipe(jade({pretty: true}))
+		.pipe(gulp.dest(tplPath + './app/'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('sass', function () {
+	gulp.src(tplPath + '/sass/**/*.sass')
+		.pipe(plumber())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(rename('build.css'))
+		.pipe(autoprefixer('last 10 versions', '> 1%', 'ie 9'))
+		.pipe(gulp.dest(tplPath + '/app/'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('server', function(){
+	browserSync.init({
+		server: {
+			baseDir: "./app"
+		}
+	});
+});
+
+gulp.task('watch', ['jade', 'sass', 'scripts', 'server'], function () {
+	watch(tplPath + '/scripts/**/*.js', function(){
+		gulp.start("scripts");
+		browserSync.reload();
+	});
+	watch(tplPath + '/jade/**/*.jade', function(){
+		gulp.start("jade");
+		browserSync.reload();
+	});
+	watch(tplPath + '/sass/**/*.sass', function(){
+		gulp.start("sass");
+		browserSync.reload();
+	});
+});
+
+gulp.task('default', ['jade', 'sass', 'scripts']);
---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+/*
+* Main file js
+*
+* @author Andrey Chechkin
+* @license GNU/AGPLv3
+* @see {@link https://www.gnu.org/licenses/agpl-3.0.txt|License}
+*/
+"use strict";
+
+require("jquery");
+var bootstrap = require("bootstrap");
+
+document.addEventListener("DOMContentLoaded", () => {
+	bootstrap;
+
+	console.log("Load");
+});
