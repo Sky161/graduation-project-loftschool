3f3a697903d321b17de47a10801ea6bbd2ef785b
Andrey Chechkin <andrey.chechkin@me.com>
2016-08-21 02:46:52 +0300

    Add modules from work api vk

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,12 @@
 * @see {@link https://www.gnu.org/licenses/agpl-3.0.txt|License}
 */
 "use strict";
+const vkApi = require("./modules/vk-api");
 
 document.addEventListener("DOMContentLoaded", () => {
-	console.log("Load");
+	vkApi.login().then(() => {
+		return vkApi.getApi("friends.get", {fields: "photo_50"})
+	}).then((response) => {
+		console.log(response);
+	}).catch((e) => { alert(`Ошибка: ${e.message}`); });
 });
